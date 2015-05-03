b0691fb00deec8a818299556e831fa85d2d05697
Matthew J. Berger <matthewjordanberger@gmail.com>
2015-05-03 16:20:40 -0700

    Update installation script and install vim-easytags

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
+# Create a symbolic link to the vimrc
 ln -s ~/.vim/vimrc ~/.vimrc
+
+# Initialize and update submodules
 cd ~/.vim
 git submodule init
 git submodule update --init --recursive
+
+# Build the YouCompletMe code completion plugin
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer --gocode-completer
+
+# Build the vimproc plugin
 cd ~/.vim/bundle/vimproc
 make
