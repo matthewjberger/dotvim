02d405a22a6ef8fbe5294d007ca9772f9b0a4ad5
Matthew J. Berger <matthewjordanberger@gmail.com>
2015-05-03 23:24:59 -0700

    Update installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+# Install dependencies
+sudo apt-get install ack-grep
+sudo apt-get install exuberant-ctags
+sudo apt-get install golang
+
 # Create a symbolic link to the vimrc
 ln -s ~/.vim/vimrc ~/.vimrc
 
