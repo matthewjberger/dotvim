38c622f68b24d99ff54ba05292dd2f6e67709c16
Matthew J. Berger <pacoishere@gmail.com>
2015-10-07 09:34:33 -0700

    Add installation script to cleanup README.MD. Comment out rarely-used plugins.

---
+++ install.sh
@@ -0,0 +1,33 @@
+# Install dependencies
+sudo apt-get install ack-grep
+sudo apt-get install silversearcher-ag
+sudo apt-get install exuberant-ctags
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev
+sudo apt-get install golang
+
+# Create a symbolic link to the vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
+
+# Initialize and update submodules
+cd ~/.vim
+git submodule init
+git submodule update --init --recursive
+
+# Install powerline fonts
+$dir="~/.vim/powerlinefonts"
+git clone https://github.com/powerline/fonts.git $dir
+if [ -d "$dir"]; then
+    cd $dir
+    ./install.sh
+    gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Ubuntu Mono derivative Powerline 12"
+    echo "If vim-airline doesn't look right, you may have to go into your terminal properties
+    and make sure that \"Use the system fixed width font\" under the General tab is unchecked."
+    rm -rf $dir
+fi
+
+# Install Vim-Plug and plugins
+cd ~/.vim
+git submodule init
+git submodule update
+vim +PlugInstall +qa
